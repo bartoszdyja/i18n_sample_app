7c8c614cf3b9970348ca85762fcc224284b477d9
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-08-26 14:07:53 +0200

    I18n example

---
+++ app/controllers/tranlation_controller.rb
@@ -0,0 +1,9 @@
+class TranlationController < ApplicationController
+  before_action :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  def index
+  end
+end
---
+++ app/helpers/tranlation_helper.rb
@@ -0,0 +1,2 @@
+module TranlationHelper
+end
